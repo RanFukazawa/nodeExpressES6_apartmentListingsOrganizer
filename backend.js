ee75024ea6727c043dfadba391442e129903483d
RanFukazawa <fukawaran413@gmail.com>
2025-10-05 23:07:20 -0700

    install node express and nodemon

--- backend.js
+++ backend.js
@@ -2,8 +2,10 @@ import express from "express";
 
 console.log("Initializing the backend...");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("frontend"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
